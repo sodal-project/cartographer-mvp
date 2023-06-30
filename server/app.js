a22a05e6845b4f5404ab35eb1c76d3f8f716febc
Tom Benbow <tom@hellomoon.net>
2023-06-30 13:34:13 -0500

    Persist json files in docker

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const cors = require('cors');
 const app = express();
 const port = 3001;
@@ -13,18 +14,46 @@ const personas = [
 app.use(cors());
 
 app.get('/personas', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.json(personas);
+  const filePath = 'data/personas.json';
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('Personas file not found. Has it been created?');
+      return;
+    }
+
+    const personasData = JSON.parse(data);
+    res.setHeader('Content-Type', 'application/json');
+    res.json(personasData);
+  });
 });
 
 app.get('/create', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.json(personas);
+  // Write a JSON file with the personas array
+  const jsonContent = JSON.stringify(personas);
+  fs.writeFile('data/personas.json', jsonContent, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error occurred while creating the file.');
+    } else {
+      res.send('JSON file created successfully.');
+    }
+  });
 });
 
-app.get('/read', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.json(personas);
+app.get('/delete', (req, res) => {
+  const filePath = 'data/personas.json';
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error occurred while deleting the file.');
+      return;
+    }
+
+    res.send('File deleted successfully.');
+  });
 });
 
 app.listen(port, () => {
