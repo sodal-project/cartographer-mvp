ae69988de81f5df1f5feae45b296f9c4def791d6
Tom Benbow <tom@hellomoon.net>
2023-07-11 12:26:39 -0500

    Add fontawesome icons

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,10 +1,13 @@
-import { Fragment, useState } from 'react'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAddressBook } from '@fortawesome/free-regular-svg-icons'
+import { faChartLine, faGears, faGear, faMap } from '@fortawesome/free-solid-svg-icons'
 
 const navigation = [
-  { name: 'Directory', href: '#', current: true },
-  { name: 'Risk Analysis', href: '#', current: false },
-  { name: 'Integrations', href: '#', current: false },
-  { name: 'Setup', href: '#', current: false },
+  { name: 'Directory', icon: faAddressBook, href: '#', current: true },
+  { name: 'Risk Analysis', icon: faChartLine, href: '#', current: false },
+  { name: 'Integrations', icon: faGears, href: '#', current: false },
+  { name: 'Setup', icon: faGear, href: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -17,12 +20,8 @@ export default function Example() {
   return (
     <>
     <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-black/10 px-6">
-          <div className="flex h-16 shrink-0 items-center">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Cartographer"
-            />
+          <div className="flex h-16 shrink-0 items-center text-indigo-500">
+            <FontAwesomeIcon icon={faMap} size="xl" />
           </div>
           <nav className="flex flex-1 flex-col pt-10">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -34,11 +33,12 @@ export default function Example() {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? 'bg-gray-800 text-white'
-                            : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                          'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                        )}
+                          ? 'bg-gray-800 text-white'
+                          : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                          'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold items-center'
+                          )}
                       >
+                        <FontAwesomeIcon icon={item.icon} size="lg" />
                         {item.name}
                       </a>
                     </li>
