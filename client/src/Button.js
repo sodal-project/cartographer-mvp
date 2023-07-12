111ca9c85ee03a18d2d64a62f052566551ac1b71
Tom Benbow <tom@hellomoon.net>
2023-07-11 20:18:56 -0500

    Refine integrations ui

--- client/src/Button.js
+++ client/src/Button.js
@@ -8,7 +8,7 @@ export default function Button({
     <button
       onClick={click}
       type="button"
-      className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+      className="block rounded-md bg-indigo-500 px-4 py-2.5 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
     >
       {label}
     </button>
--- client/src/Integrations.js
+++ client/src/Integrations.js
@@ -3,11 +3,15 @@ import Button from './Button';
 
 const data = [
   {
-    label: 'Github @tbenbow',
+    id: '1',
+    type: 'Github',
+    name: '@tbenbow',
     value: 'AKDO832l987LS780L978345lkasO'
   },
   {
-    label: 'Github @tbenbowadmin',
+    id: '2',
+    type: 'Github',
+    name: '@tbenbowadmin',
     value: 'BNSDLU8230267LKSDF037120kasl'
   }
 ]
@@ -17,7 +21,7 @@ const Field = ({
   value
 }) => {
   return (
-    <div className="sm:col-span-2 sm:col-start-1 w-60">
+    <div className="flex-1">
       <label htmlFor="city" className="block text-sm font-medium leading-6 text-white">
         {label}
       </label>
@@ -26,9 +30,39 @@ const Field = ({
           type="text"
           name="city"
           id="city"
+          value={value}
           autoComplete="address-level2"
-          className="block w-full rounded-md border-0 bg-white/5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 bg-white/5 p-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
         />
+      </div>
+    </div>
+  )
+}
+
+const Form = ({saveClick, cancelClick, name, value, type, id}) => {
+  return (
+    <div className="flex gap-4 w-full max-w-2xl items-end">
+      <div className="w-28">
+        <label htmlFor="country" className="block text-sm font-medium leading-6 text-white">
+          Type
+        </label>
+        <div className="mt-2">
+          <select
+            id="type"
+            name="type"
+            className="block w-full rounded-md border-0 bg-white/5 py-2.5 px-1 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+          >
+            <option>Github</option>
+            <option>Google</option>
+            <option>Slack</option>
+          </select>
+        </div>
+      </div>
+      <Field label="name" value={name} />
+      <Field label="Token" value={value} />
+      <div className='flex gap-3 mt-6'>
+        <Button label="Save" click={() => { saveClick() }} />
+        <Button label="Cancel" click={() => { cancelClick() }} />
       </div>
     </div>
   )
@@ -36,73 +70,52 @@ const Field = ({
 
 export default function Integrations() {
   const [mode, setMode] = useState('view');
+  const [editId, setEditId] = useState('');
 
-  const handleModeChange = (mode) => {
+  const handleModeChange = (mode, id="") => {
     setMode(mode);
+    setEditId(id);
   };
 
   return (
     <div className="bg-gray-900 p-10">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-2xl font-semibold leading-6 text-white">Integrations</h1>
-          {mode}
+          <h1 className="text-3xl font-semibold leading-6 text-white">Integrations</h1>
           <div className="mt-12 mb-6 divide-y divide-gray-800">
-            
-            { data.map((item) => (
-              <div className="flex text-white py-3 hover:bg-violet-600/10 cursor-pointer">
-                <div className='w-80'>{item.label}</div>
-                <div>************************</div>
-                <div className="ml-16">
-                  <a href="#" className="text-indigo-400 hover:text-indigo-300">
-                    Edit<span className="sr-only"></span>
-                  </a>
-                </div>
-                <div className="ml-4">
-                  <a href="#" className="text-indigo-400 hover:text-indigo-300">
-                    Delete<span className="sr-only"></span>
-                  </a>
-                </div>
-              </div>
-            ))}
-            
-            {mode === 'edit' && 
+            {data.map((item) => (
               <div>
-                <div className="flex gap-4 items-end mt-8">
-                  <div className="w-40">
-                    <label htmlFor="country" className="block text-sm font-medium leading-6 text-white">
-                      Type
-                    </label>
-                    <div className="mt-2">
-                      <select
-                        id="country"
-                        name="country"
-                        autoComplete="country-name"
-                        className="block w-full rounded-md border-0 bg-white/5 py-2.5 px-1 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
-                      >
-                        <option>Github</option>
-                        <option>Google</option>
-                        <option>Slack</option>
-                      </select>
+                {editId === item.id &&
+                  <div className="mb-6 mt-2">
+                    <div className='flex gap-3 items-end'>
+                      <Form name={item.name} value={item.value} id={item.id} saveClick={() => { handleModeChange('view') }} cancelClick={() => { handleModeChange('view') }}/>
                     </div>
                   </div>
-                  <Field label="name" />
-                  <Field label="value" />
-                  <div className='relative -top-1'>
+                }
+                {editId !== item.id &&
+                  <div className="flex gap-4 text-white py-3 cursor-pointer">
+                    <div className='w-28'>{item.type}</div>
+                    <div className='w-48'>{item.name}</div>
+                    <div className='w-36'>*******************</div>
+                    <a onClick={() => { handleModeChange('view', item.id) }} className="text-indigo-400 hover:text-indigo-300 ml-6">
+                      Edit
+                    </a>
+                    <a href="#" className="text-indigo-400 hover:text-indigo-300">
+                      Delete
+                    </a>
                   </div>
-                </div>
+                }
               </div>
+            ))}
+          </div>
+          <div className="mt-12">
+            {mode === 'view' &&
+              <Button label="Add" click={() => { handleModeChange('add') }} />
+            }
+            {mode === 'add' &&
+              <Form name="" value="" id="" saveClick={() => { handleModeChange('view') }} cancelClick={() => { handleModeChange('view') }}/>
             }
           </div>
-          {mode === 'view' && 
-            <Button label="Add" click={() => { handleModeChange('edit') }} />
-          }
-          {mode === 'edit' && 
-            <div className='flex gap-3'>
-              <Button label="Save" click={() => { handleModeChange('view') }} />
-              <Button label="Cancel" click={() => { handleModeChange('view') }} />
-            </div>
-          }
         </div>
       </div>
     </div>
