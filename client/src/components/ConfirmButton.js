1b6517024a1fe9840ebbab596c6a59569f40ac7f
Tom Benbow <tom@hellomoon.net>
2023-07-19 14:01:04 -0500

    View persona details (#39)

--- client/src/components/ConfirmButton.js
+++ client/src/components/ConfirmButton.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
 import Button from './Button';
 
 export default function ConfirmButton({
-  label,
   click,
-  type = 'solid',
   submit = false,
   disabled = false,
-  icon = null
 }) {
   const [confirming, setConfirming] = useState(false);
 
   return (
-    <div className="relative">
-      <Button type={type} label={label} icon={icon} disabled={disabled} click={() => { setConfirming(true)}} />
-      {confirming &&
-        <div className="absolute -right-2 top-1/2 transform -translate-y-1/2 flex gap-4 items-center bg-gray-900 border border-gray-700 rounded-full px-3 py-0.5 shadow-black/30 shadow-md">
-          <Button type="link" label={`delete`} click={click} submit={submit} />
-          <div className="absolute w-px top-1 bottom-1 left-1/2 transfrom -translate-x-px bg-gray-700" />
-          <Button type="link" label="cancel" click={() => { setConfirming(false)}} />
-        </div>
-      }
+    <div>
+      <Button type="link" icon={faTrash} disabled={disabled} click={() => { setConfirming(true)}} />
+      <div className="relative">
+        {confirming &&
+          <div className="absolute -right-2 bottom-0 transform translate-y-0.5 flex gap-4 items-center bg-gray-900 border border-gray-700 rounded-full px-3 py-0.5 shadow-black/30 shadow-md">
+            <Button type="link" label={`delete`} click={click} submit={submit} />
+            <div className="absolute w-px top-1 bottom-1 left-1/2 transfrom -translate-x-px bg-gray-700" />
+            <Button type="link" label="cancel" click={() => { setConfirming(false)}} />
+          </div>
+        }
+      </div>
     </div>
   )
 }
