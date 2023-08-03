a059d48b730e8fefdfd7f88fa2423aee85a8fa79
Tom Benbow <tom@hellomoon.net>
2023-08-03 01:05:31 -0500

    Style updates

--- client/src/components/Bubble.js
+++ client/src/components/Bubble.js
@@ -6,7 +6,7 @@ export default function Bubble({
   className
 }) {
   return (
-    <div className={`${className} shadow-xl p-4 w-80 bg-gray-900 border border-gray-600 flex flex-col items-start gap-2`}>
+    <div className={`${className} shadow-md shadow-black rounded-md p-4 w-80 bg-gray-900 border border-gray-600 flex flex-col items-start gap-2`}>
       {title && (
         <h4 className="text-white text-sm text-center w-full mb-2">{title}</h4>
       )}
