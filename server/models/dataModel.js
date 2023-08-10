408be821532006974457dc622e556ae0b252336b
Tom Benbow <tom@hellomoon.net>
2023-08-09 23:02:17 -0500

    Refactor/data folder setup (#84)
    * Refactor how data folders are created
    
    * Rename data controller and model

---
+++ server/models/dataModel.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+function setupDataFolder() {
+  return new Promise(async (resolve, reject) => {
+    const folderPath = path.join(__dirname, `../data`)
+
+    // Data folder already exists
+    if (fs.existsSync(folderPath)) {
+      resolve('data folder already exists')
+
+    // No data folder, create it
+    } else {
+      try {
+        await fs.promises.mkdir(folderPath)
+        resolve('created data folder')
+      } catch (error) {
+        reject(`Error creating data folder: ${error}`)
+      }
+    }
+  })
+}
+
+function setupDataSubFolders(response) {
+  return new Promise(async (resolve, reject) => {
+
+    // Determine folders to create
+    const requiredFolders = ['cache', 'sets', 'keys', 'logs', 'csv']
+    const foldersToCreate = requiredFolders.filter(folder => {
+      let folderPath = path.join(__dirname, `../data/${folder}`)
+      return !fs.existsSync(folderPath)
+    })
+
+    // Check if we need to create folders
+    if (foldersToCreate.length === 0) {
+      resolve (`${response}, sub folders already exist`)
+    }
+    
+    // Create Folder Promises
+    const foldersToCreatePromises = foldersToCreate.map(folder => {
+      let folderPath = path.join(__dirname, `../data/${folder}`)
+      return fs.promises.mkdir(folderPath)
+    })
+
+    // Wait for all folders to be created
+    try {
+      await Promise.all(foldersToCreatePromises)
+      resolve (`${response}, sub folders created`)
+    } catch (error) {
+      reject('Error creating sub folders in data')
+    }
+  })
+}
+
+module.exports = {
+  setupDataFolder,
+  setupDataSubFolders
+};
