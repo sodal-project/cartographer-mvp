f4cc4e3e8b4a2b828568c60fb825f85c55e7a148
Tom Benbow <tom@hellomoon.net>
2023-08-30 16:45:42 -0500

    Add outside click for filter UI

--- client/src/components/Discovery/DiscoveryAdd.js
+++ client/src/components/Discovery/DiscoveryAdd.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import Button from '../Button';
+import OutsideClick from '../OutsideClick';
 import DiscoveryAddControl from './DiscoveryAddControl';
 import DiscoveryAddFilter from './DiscoveryAddFilter';
 import DiscoveryAddMatch from './DiscoveryAddMatch';
 
-export default function DiscoveryAdd({ onSave, parentId, initialMode = 'view' }) {
+export default function DiscoveryAdd({ onSave, parentId, initialMode = '' }) {
   const [mode, setMode] = useState(initialMode);
 
   const addItem = () => {
     if (mode === 'add') {
-      setMode('view');
+      setMode('');
     } else {
       setMode('add');
     }
@@ -18,32 +19,30 @@ export default function DiscoveryAdd({ onSave, parentId, initialMode = 'view' })
 
   const saveForm = (data) => {
     onSave(data, parentId);
-    setMode('view');
+    setMode('');
   }
 
   return (
     <div className="relative flex justify-center pt-3">
-      <Button icon={faPlus} type="outline-circle-small" click={() => { addItem() }} />
-
-      {/* Add Item Menu */}
-      {mode === 'add' && (
-        <div className="absolute top-16 left-1/2 -translate-x-1/2 z-10 shadow-md shadow-black p-4 w-40 bg-gray-900 border border-gray-600 flex flex-col items-start gap-2">
-          <Button label="Add Filter" type="link" click={() => { setMode('add-filter') }} />
-          <Button label="Add Control" type="link" click={() => { setMode('add-control') }} />
-          <Button label="Add Match" type="link" click={() => { setMode('add-match') }} />
-        </div>
-      )}
-
-      {/* Add Item UI */}
-      {mode === 'add-filter' && (
-        <DiscoveryAddFilter onSave={saveForm} onCancel={() => setMode("view")} />
-      )}
-      {mode === 'add-control' && (
-        <DiscoveryAddControl onSave={saveForm} onCancel={() => setMode("view")} />
-      )}
-      {mode === 'add-match' && (
-        <DiscoveryAddMatch onSave={saveForm} onCancel={() => setMode("view")} />
-      )}
+      <OutsideClick onClickOutside={() => { setMode('') }}>
+        <Button icon={faPlus} type="outline-circle-small" click={() => { addItem() }} />
+        {mode === 'add' && (
+          <div className="absolute top-16 left-1/2 -translate-x-1/2 z-10 shadow-md shadow-black p-4 w-40 bg-gray-900 border border-gray-600 flex flex-col items-start gap-2">
+            <Button label="Add Filter" type="link" click={() => { setMode('add-filter') }} />
+            <Button label="Add Control" type="link" click={() => { setMode('add-control') }} />
+            <Button label="Add Match" type="link" click={() => { setMode('add-match') }} />
+          </div>
+        )}
+        {mode === 'add-filter' && (
+          <DiscoveryAddFilter onSave={saveForm} onCancel={() => setMode('')} />
+        )}
+        {mode === 'add-control' && (
+          <DiscoveryAddControl onSave={saveForm} onCancel={() => setMode('')} />
+        )}
+        {mode === 'add-match' && (
+          <DiscoveryAddMatch onSave={saveForm} onCancel={() => setMode('')} />
+        )}
+      </OutsideClick>
     </div>
   )
 }
