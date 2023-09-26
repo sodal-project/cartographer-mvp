7925bde3b694751ffa25fc865b38463104ab2d31
Tom Benbow <tom@hellomoon.net>
2023-09-26 15:27:14 -0500

    Add amazon logo to table view

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,7 +1,7 @@
 import ConfirmButton from '../components/ConfirmButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faUsers, faBuilding, faEnvelope, faLinkSlash } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faGoogle, faSlack } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faGoogle, faSlack, faAmazon } from '@fortawesome/free-brands-svg-icons'
 import Button from './Button';
 
 export default function Table({
@@ -17,6 +17,7 @@ export default function Table({
     tableLabels.push('')
   }
   const platformLogos = {
+    aws: faAmazon,
     github: faGithub,
     google: faGoogle,
     email: faEnvelope,
