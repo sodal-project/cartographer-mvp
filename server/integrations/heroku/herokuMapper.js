1b0e0d9e875769ab0dd14f32f5785d814112b2f3
Tom Benbow <tom@hellomoon.net>
2023-10-06 16:27:39 -0500

    Heroku integration

---
+++ server/integrations/heroku/herokuMapper.js
@@ -0,0 +1,125 @@
+/*
+  MAPPERS
+  Each function in this file takes an api response and maps it to
+  the personas schema passed to the Persona.addToDatabase function which adds
+  data to the database.
+*/
+
+const teamPersonas = (response) => {
+  const personas = response.map(item => {
+    return ({
+      standardProps: {
+        id: item.id,
+        status: "active",
+        platform: "heroku",
+        type: "team",
+        friendlyName: item.name,
+      },
+      customProps: {
+        name: item.name,
+        role: item.role,
+        accountType: item.type,
+        identityProvider: item.identity_provider,
+        membershipLimit: item.membership_limit,
+      }
+    })
+  })
+  return personas
+}
+
+const memberPersonas = (response) => {
+  const personas = response.map(item => {
+    return ({
+      standardProps: {
+        id: item.id,
+        status: "active",
+        platform: "heroku",
+        type: "account",
+        friendlyName: item.user?.name || item.email,
+      },
+      customProps: {
+        name: item.user?.name || item.email,
+        authenticationMin: item.two_factor_authentication ? 2 : 1,
+      },
+      email: item.email,
+      controls: [
+        {
+          role: item.role,
+          upn: `upn:heroku:team:${item.teamId}`,
+        },
+      ],
+      obeys: [
+        {
+          role: item.role,
+          upn: `upn:email:account:${item.email}`,
+        }
+      ]
+    })
+  })
+  return personas
+}
+
+const appPersonas = (response) => {
+  const personas = response.map(item => {
+    return ({
+      standardProps: {
+        id: item.id,
+        status: "active",
+        platform: "heroku",
+        type: "app",
+        friendlyName: item.name,
+      },
+      customProps: {
+        name: item.name,
+        region: item.region?.name,
+        webUrl: item.web_url,
+        gitUrl: item.git_url,
+      },
+      email: item.owner?.email,
+      obeys: [
+        {
+          role: 'admin',
+          upn: `upn:heroku:team:${item.team?.id}`,
+        },
+        {
+          role: 'superadmin',
+          upn: `upn:email:account:${item.owner?.email}`,
+        }
+      ]
+    })
+  })
+  return personas
+}
+
+const collaboratorPersonas = (response) => {
+  const personas = response.map(item => {
+    return ({
+      standardProps: {
+        id: item.id,
+        status: "active",
+        platform: "heroku",
+        type: "collaborator",
+        friendlyName: item.user.email,
+        name: item.user?.email,
+      },
+      customProps: {
+        name: item.user?.email,
+      },
+      email: item.user?.email,
+      controls: [
+        {
+          role: item.role,
+          upn: `upn:email:account:${item.user?.email}`,
+        },
+      ]
+    })
+  })
+  return personas
+}
+
+module.exports = {
+  teamPersonas,
+  memberPersonas,
+  appPersonas,
+  collaboratorPersonas
+};
