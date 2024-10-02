85e843fe5217506d23a619e0f3d33f0b00950048
Andy Schwab <59572086+andyschwab@users.noreply.github.com>
2024-10-02 16:04:02 -0400

    initial v1 database connector

---
+++ v1staging/dbConnector.js
@@ -0,0 +1,158 @@
+require('dotenv').config();
+const neo4j = require('neo4j-driver');
+
+const Config = {
+  // db_host: `bolt://${process.env.INSTANCE_NAME_DB}:${process.env.NEO4J_BOLT_PORT}`,
+  db_host: `bolt://localhost:7687`,
+  db_username: process.env.NEO4J_USERNAME,
+  db_password: process.env.NEO4J_PASSWORD,
+  healthCheck: false,
+}
+
+const HealthQueries = {
+  "getCount": "MATCH (n) RETURN count(n) as count",
+  "setContraintUPN": "CREATE CONSTRAINT contraint_upn IF NOT EXISTS FOR (n:Persona) REQUIRE n.upn IS UNIQUE",
+  "setContraintSource": "CREATE CONSTRAINT contraint_source IF NOT EXISTS FOR (n:Source) REQUIRE n.source IS UNIQUE",
+  "setIndexType": "CREATE INDEX index_persona_type IF NOT EXISTS FOR (n:Persona) ON (n.type)",
+  "setIndexPlatform": "CREATE INDEX index_persona_platform IF NOT EXISTS FOR (n:Persona) ON (n.platform)",
+}
+
+const healthCheck = async () => {
+  if(Config.healthCheck) {
+    return true;
+  } else {
+    let driver = null;
+    let session = null;
+
+    try {
+      // connect to the default database with the admin login
+      console.log('Health Check: CHECK - connecting to the database');
+      driver = neo4j.driver(Config.db_host, neo4j.auth.basic(Config.db_username, Config.db_password));
+      session = driver.session();
+      let result = await session.run(HealthQueries.getCount);
+      let count = result.records[0].get('count');
+      console.log(`Health Check: OK - Connected, ${count} nodes in the database`);
+
+      // verify that indexes and constraints are set
+      console.log('Health Check: CHECK - setting constraints and indexes on the database');
+      await session.run(HealthQueries.setContraintUPN);
+      await session.run(HealthQueries.setContraintSource);
+      await session.run(HealthQueries.setIndexType);
+      await session.run(HealthQueries.setIndexPlatform);
+      console.log('Health Check: OK - Constraints and Indexes are set on the database');
+
+      // return true if all checks pass
+      await session.close();
+      await driver.close();
+      Config.healthCheck = true;
+
+    } catch (error) {
+      console.error('Health Check error:', error);
+      await session.close();
+      await driver.close();
+      return false;
+    }
+    return true;
+  }
+}
+
+const dbQueryArray = async (queryArray) => {
+  if(!(await healthCheck())) {
+    console.error('Health Check failed, unable to process query array.');
+    return null;
+  }
+
+  console.log('--- Process dbQueryArray with ' + queryArray.length + ' queries...');
+
+  const driver = neo4j.driver(Config.db_host, neo4j.auth.basic(Config.db_username, Config.db_password), { encrypted: false });
+  const session = driver.session();
+  const startTime = performance.now();
+  const transaction = session.beginTransaction();
+  let response = {};
+
+  try {
+    const tPromisesArray = [];
+
+    for(let q in queryArray){
+      let curQueryObj = queryArray[q];
+      let curQueryString = curQueryObj["query"];
+      let curQueryValues = curQueryObj["values"];
+    
+      let tPromise = transaction.run(curQueryString, curQueryValues);
+      tPromisesArray.push(tPromise);
+    }
+
+    console.log("All query transactions submitted, waiting for completion...");
+    // wait for all transactions to finish
+    await Promise.all(tPromisesArray).then(
+      (results) => {
+        response = results;
+        console.log("--- End processing query array. Completed " + results.length + " transactions.");
+      }
+    );
+    await transaction.commit();
+    const duration = performance.now() - startTime;
+    console.log(`Processed ${queryArray.length} queries in ${duration} milliseconds.`);
+
+  } catch (error) {
+    console.error(`Error processing query array: ${error}`)
+  } finally {
+    await session.close()
+    await driver.close()
+  }
+  return response;
+}
+
+const dbQueryPaged = async (query, page=1, pageSize=1500, optionalParams) => {
+  if(!(await healthCheck())) {
+    console.error('Health Check failed, unable to process paged query.');
+    return null;
+  }
+
+  const driver = neo4j.driver(Config.db_host, neo4j.auth.basic(Config.db_username, Config.db_password));
+  const session = driver.session();
+
+  const skip = neo4j.int((page - 1) * pageSize);
+  const limit = neo4j.int(pageSize);
+  const params = { skip, limit, ...optionalParams};
+
+  try {
+    const result = await session.run(query, params);
+    return result;
+  } catch (error) {
+    console.error('Error processing paged query:', error);
+    throw error;
+  } finally {
+    session.close();
+    driver.close();
+  }
+}
+
+const dbQueryRaw = async (query, optionalParams) => {
+  if(!(await healthCheck())) {
+    console.error('Health Check failed, unable to process raw query.');
+    return false;
+  }
+
+  const driver = neo4j.driver(Config.db_host, neo4j.auth.basic(Config.db_username, Config.db_password));
+  const session = driver.session();
+
+  try {
+    let result = await session.run(query, optionalParams);
+    await session.close();
+    await driver.close();
+    return result.records;
+
+  } catch (error) {
+    console.error('Error processing raw query:', error);
+    await session.close();
+    await driver.close();
+    return false;
+  }
+}
+
+module.export = {
+  dbQueryArray,
+  dbQueryPaged,
+  dbQueryRaw
+}
